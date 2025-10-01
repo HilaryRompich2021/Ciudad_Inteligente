8caccf2cd55984065d7f3c443a1b999f5d73e3e4
Rzapet25 <ronaldmanriquer@gmail.com>
2025-10-01 12:28:01 -0600

    infra: mejora script de creación de topics y docker-compose

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -1,9 +1,7 @@
-
-#!/usr/bin/env bash
+#!/bin/bash
 # Script para crear los topics requeridos en Kafka
 
 KAFKA_BROKER="kafka:9092"
 
-# Crear topics 
-docker exec -i $KAFKA_CONTAINER kafka-topics.sh --create --if-not-exists --topic t01.events.standardized --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=259200000
-docker exec -i $KAFKA_CONTAINER kafka-topics.sh --create --if-not-exists --topic t01.correlated.alerts --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=604800000
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic t01.events.standardized --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=259200000
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic t01.correlated.alerts --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=604800000
