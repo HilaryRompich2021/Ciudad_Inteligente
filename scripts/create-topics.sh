d1ae97ffbe7909b21b6aa474d8cd86feb4aed290
Rzapet25 <ronaldmanriquer@gmail.com>
2025-10-05 19:40:37 -0600

    scripts: actualizar script de creación de topics Kafka

--- scripts/create-topics.sh
+++ scripts/create-topics.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Script para crear los topics requeridos en Kafka
+# Script para crear los topics requeridos en Kafka 
 
 KAFKA_BROKER="kafka:9092"
 
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic t01.events.standardized --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=259200000
-/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic t01.correlated.alerts --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=604800000
+# Topics según especificación de la guía (sección 4)
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic events.standardized --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=259200000
+/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic correlated.alerts --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=604800000
+
+# Topic opcional para DLQ (Dead Letter Queue)
+#/opt/bitnami/kafka/bin/kafka-topics.sh --create --if-not-exists --topic events.dlq --bootstrap-server $KAFKA_BROKER --partitions 3 --replication-factor 1 --config retention.ms=259200000
